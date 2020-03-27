1e0022ef0ba53684b39ec4990b29b772e8840644
mutugiii <mutugimutuma@gmail.com>
2020-03-27 18:22:29 +0300

    Write serializers

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Profile, Post, Hood, EmergencyService
+from .models import User, Bussiness, Hood, EmergencyService
 
 admin.site.register(User)
 admin.site.register(Hood)
 admin.site.register(EmergencyService)
+admin.site.register(Bussiness)
---
+++ api/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import User, Post, Profile, Hood, EmergencyService, Bussiness
+from django.contrib.auth.hashers import make_password
+
+class UserSerializer(serializers.ModelSerializer):
+    '''Serializer for user Model'''
+    email = serializers.EmailField()
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+    
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+class HoodSerializer(serializers.ModelSerializer):
+    '''Serializer for Hood Model'''
+    admin = serializers.StringRelatedField(read_only=True)
+    class meta:
+        model = Hood
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    '''Serializer for profile Model'''
+    user = serializers.StringRelatedField(read_only=True)
+    hood = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    '''Serializer for Post Model'''
+    user = serializers.StringRelatedField(read_only=True)
+    hood = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class BussinessSerializer(serializers.ModelSerializer):
+    '''Serializer for Bussiness class'''
+    hood = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Bussiness
+        fields = '__all__'
+
+
+class EmergencyServiceSerializer(serializers.ModelSerializer):
+    '''Serializer for Bussiness class'''
+    hood = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = EmergencyService
+        fields = '__all__'
