8cdc18081e7735fa514274245d349c78dfdb5550
mutugiii <mutugimutuma@gmail.com>
2020-03-30 23:07:20 +0300

    Add cors whitelist

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,3 +139,10 @@ cloudinary.config(
   api_secret = os.environ.get('API_SECRET'),
 )
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:5500",
+
+]
