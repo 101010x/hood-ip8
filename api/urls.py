ceb65dacdfa6510a18317f242dcb76de8d5ff7a2
mutugiii <mutugimutuma@gmail.com>
2020-03-31 22:24:11 +0300

    Remove login required decorator

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
 from rest_framework_simplejwt import views as jwt_views
 from .views import *
 
@@ -7,13 +6,13 @@ urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterUser.as_view(), name='register'),
-    path('hoods/', login_required(HoodCreateView.as_view()), name='hoods'),
-    path('hood/<str:search_term>/', login_required(HoodDetailsView.as_view()), name='specifichood'),
-    path('join/<str:hood_name>/<str:user_name>', login_required(UpdateHoodOptionJoinView.as_view()), name='joinhood'),
-    path('admin/set/<str:hood_name>/<str:user_name>', login_required(UpdateHoodAdminView.as_view()), name='sethoodadmin'),
-    path('profiles/', login_required(ProfileCreateView.as_view()), name='profiles'),
-    path('profile/<str:user_name>', login_required(ProfileDetailsView.as_view()), name='specificprofile'),
-    path('posts/', login_required(PostListCreateView.as_view()), name='posts'),
-    path('bussinesses/', login_required(BussinessListCreateView.as_view()), name='bussinesses'),
-    path('services/', login_required(EmergencyServiceListCreateView.as_view()), name='emergencyservice'),
+    path('hoods/', HoodCreateView.as_view(), name='hoods'),
+    path('hood/<str:search_term>/', HoodDetailsView.as_view(), name='specifichood'),
+    path('join/<str:hood_name>/<str:user_name>', UpdateHoodOptionJoinView.as_view(), name='joinhood'),
+    path('admin/set/<str:hood_name>/<str:user_name>', UpdateHoodAdminView.as_view(), name='sethoodadmin'),
+    path('profiles/', ProfileCreateView.as_view(), name='profiles'),
+    path('profile/<str:user_name>', ProfileDetailsView.as_view(), name='specificprofile'),
+    path('posts/', PostListCreateView.as_view(), name='posts'),
+    path('bussinesses/', BussinessListCreateView.as_view(), name='bussinesses'),
+    path('services/', EmergencyServiceListCreateView.as_view(), name='emergencyservice'),
 ]
